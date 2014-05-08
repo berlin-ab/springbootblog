0df10ca5b412990218de619b195bd28e5fffca9b
Adam Berlin <pair+aberlin@pivotallabs.com>
2014-05-08 12:26:26 -0400

    A running spring boot application.

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -1,7 +1,9 @@
 package example;
 
+import org.springframework.boot.SpringApplication;
+
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
