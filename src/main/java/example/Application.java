a2efc50e592d96ce8d04b4e6d9b294812ed0f5f2
Adam Berlin <pair+aberlin@pivotallabs.com>
2014-05-08 18:37:32 -0400

    Needed to create a PropertySourcesPlaceholderConfigurer bean

--- src/main/java/example/Application.java
+++ src/main/java/example/Application.java
@@ -2,12 +2,20 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan
 public class Application {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer configurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
         Thing thing = context.getBean(Thing.class);
